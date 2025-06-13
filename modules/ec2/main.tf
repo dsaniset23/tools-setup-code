79dc6961522e1f1a5545396918a7591f2d2ab891
durga.prem.sanisetty <prem.sanisetty@gmail.com>
2025-06-13 21:13:12 +0530

    egress location is wrong

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -26,11 +26,12 @@ resource "aws_security_group" "sg" {
   }
 
   egress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "TCP"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
 
   tags = {
     Name = "${var.tool_name}-sg"
