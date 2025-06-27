151851cbb606a3367be86da07cb3cd880a4e714c
durga.prem.sanisetty <prem.sanisetty@gmail.com>
2025-06-27 16:54:39 +0530

    Iam changes

---
+++ modules/ec2/iam.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_role" "role" {
+  count = length(var.policy_list) > 0 ? 1 : 0
+  name = "${var.tool_name}-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "${var.tool_name}-role-policy"
+  }
+}
+
+resource "aws_iam_role_policy" "role_policy" {
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = var.policy_list
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })  role   = aws_iam_role.role.id
+}
+
+resource "aws_iam_instance_profile" "instance_" {}
