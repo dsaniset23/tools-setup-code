19d6d1f3f0d9276188e58ac5bcf609a3ede227fd
dsaniset <prem.sanisetty@gmail.com>
2026-02-02 17:11:44 +0530

    added necessary logic to deploy certain services

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "ec2" {
-  for_each = {
-    for name, config in var.tools : name => config
-          if contains(toset(var.to_deploy), name)
-  }
-  # for_each = var.tools
+  # for_each = {
+  #   for name, config in var.tools : name => config
+  #         if contains(toset(var.to_deploy), name)
+  # }
+  for_each = var.tools
   source = "./modules/ec2"
   app_port      = each.value["app_port"]
   domain_name   = var.domain_name
